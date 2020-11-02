0bb1448bb8aa6a7be35536aec436f731bcec7ca0
Vari0n <Kenly_P@yahoo.ca>
2020-11-02 19:52:54 +0000

    transfered the eqArraysEqual test cases to an individual file and added require statement to run the tests properly

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+
+assertArraysEqual([1,2,3], [1,4,3]);
+assertArraysEqual([4,6,8] , [4,6,8]);
+assertArraysEqual(['Hello', "World"], ['Hello', "World"
+]);
