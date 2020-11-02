69f3209853d71fa02e0219d357769ab992a87116
Vari0n <Kenly_P@yahoo.ca>
2020-11-02 18:59:19 +0000

    modified the head.js to be exportable to the rest of the lotide directory

--- head.js
+++ head.js
@@ -1,28 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-/*
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(2, 200);
-*/
 
 const head = function(array) {
-  // let first = array[0];
-  // for(let i = 0; i < array.length;i++) {
-  // if(array[i] < first){
-  //  first = array[i]; 
-  //  }
-  // } 
  return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]),5);
-assertEqual(head([]),undefined);
+module.exports = head;
