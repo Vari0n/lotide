0e39f2f1f40475bfc2b8d49e3c82a7fefab6c343
Vari0n <Kenly_P@yahoo.ca>
2020-11-02 19:05:49 +0000

    modified the file with module.exportsstatment to have the function be exportable

--- tail.js
+++ tail.js
@@ -14,16 +14,5 @@ const tail = function(array) {
   return final;
 };
 
+module.exports = tail;
 
-
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-const empty = [];
-tail(empty);
-assertEqual(empty.length, 0);
-
-const single = ["hey"];
-tail(single);
-assertEqual(single.length, 1);
